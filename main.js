cc82196441abd6ef97fbd6fd381b7256dbdd6ae6
Braden Lawrence <b@bradenlawrence.com>
2020-03-12 13:31:09 -0400

    remove comment

--- main.js
+++ main.js
@@ -2,7 +2,6 @@ const { createStore } = Redux;
 const ADD_CREW = "ADD_CREW"
 const DELETE_CREW = "DELETE_CREW"
 
-// Your initial state, reducer, constants, action creators, and JS code should go here!
 // Action Creators
 const welcomeAboard = (name) => {
   return {
