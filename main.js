f081c21813980a72450cf7430a1216a070fb1b60
Braden Lawrence <b@bradenlawrence.com>
2020-03-12 13:30:07 -0400

    add action creators

--- main.js
+++ main.js
@@ -3,3 +3,16 @@ const ADD_CREW = "ADD_CREW"
 const DELETE_CREW = "DELETE_CREW"
 
 // Your initial state, reducer, constants, action creators, and JS code should go here!
+// Action Creators
+const welcomeAboard = (name) => {
+  return {
+    type: ADD_CREW,
+    name: name
+  }
+}
+const walkThePlank = (name) => {
+  return {
+    type: DELETE_CREW,
+    name: name
+  }
+}
