b4c981c6e73b6a2519c87134627d9c726ce66ed9
Braden Lawrence <b@bradenlawrence.com>
2020-03-12 13:29:50 -0400

    add redux action constants

--- main.js
+++ main.js
@@ -1,3 +1,5 @@
 const { createStore } = Redux;
+const ADD_CREW = "ADD_CREW"
+const DELETE_CREW = "DELETE_CREW"
 
 // Your initial state, reducer, constants, action creators, and JS code should go here!
